06fce3db7d059bc3a0544b7383acfe63e4a122e3
Ridwan <ridwanalamsyah96@gmail.com>
2026-09-10 02:19:04 +0700

    WIP: payment pending page

---
+++ resources/views/page/payment-pending.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.index')
+@section('content')
+<section id="about-us" class="padding-large">
+    <div class="container-fluid padding-side" data-aos="fade-up">
+        <h3 class="display-3 text-center fw-normal col-lg-4 offset-lg-4" style="border: 1px ">Payment Pending</h3>
+        <div class="row align-items-start mt-3 mt-lg-5">
+            <div class="col-lg-6">
+                    <p>Welcome to Hotel Mellow, where comfort meets tranquility. Nestled in the heart of a bustling
+                        city, our
+                        hotel offers a peaceful retreat for both business and leisure travelers. With modern amenities,
+                        and a
+                        warm, inviting atmosphere, we strive to make your stay with us.</p>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\Front\HomeController@getIndex')->name('home.index');
-Route::post('/payment/process', 'App\Http\Controllers\Front\HomeController@postPaymentProcess')->name('home.payment');
+Route::post('/payment/process', 'App\Http\Controllers\Front\HomeController@postPaymentProcess')->name('home.payment.process');
+Route::get('/payment/pending/{order_no}', 'App\Http\Controllers\Front\HomeController@getPaymentPending')->name('home.payment.pending');
